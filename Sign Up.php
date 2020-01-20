c2b966750de60e20d6b81f4e1042375580e6e6bf
Nevin456 <59451343+Nevin456@users.noreply.github.com>
2020-01-20 20:13:03 +0530

    Add files via upload

---
+++ Sign Up.php
@@ -0,0 +1,179 @@
+<?php
+
+require_once "config.php";
+ 
+
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+ 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+   
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } else{
+       
+        $sql = "SELECT id FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+           
+            $param_username = trim($_POST["username"]);
+            
+       
+            if(mysqli_stmt_execute($stmt)){
+             
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        
+        mysqli_stmt_close($stmt);
+    }
+    
+    
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+   
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            
+            
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); 
+            
+            
+            if(mysqli_stmt_execute($stmt)){
+                
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        
+        mysqli_stmt_close($stmt);
+    }else{
+    	echo"Something's wrong with the query:".mysqli_error($link);
+    }
+    
+    
+   
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Apex World-Sign Up</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body style="background-color: #999999;">
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="login100-more" style="background-image: url('images/apexsign.jpg');"></div>
+
+			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
+				<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+			</div>
+		</div>
+	</div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,96 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>index</title>
+
+        <style type="text/css">
+			a{
+				text-underline-position: below;
+			}
+			
+			a-hover{
+				
+			}
+            
+ body{ font: 14px sans-serif; text-align: center; }
+           
+            
+
+        </style>
+        <link href="css/Override.css" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap-4.3.1.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+<div class="page-header">
+        <h1 style="color: red;"><center>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></center></h1>
+    </div>
+    <p>
+        
+
+        <ul class="fly-in-text hidden"><a href="Apex World2.lk.html" style="color:red;">
+
+               <center><li>WELCOME</li>
+            
+		    <li>TO</li></center><br>
+            <center><li>A</li>
+            <li>P</li>
+            <li>E</li>
+            <li>X</li>
+			<li> </li>
+			<li>W</li>
+			<li>O</li>
+			<li>R</li>
+			<li>L</li> 
+		    <li>D</li></center>
+	        <li>&nbsp;</li>
+			<center><li>S</li>
+			<li>T</li>
+			<li>O</li>
+			<li>R</li>
+	        <li>E</li></center>
+			<br></a>
+    <center><a href="Apex World2.lk.html" style="color:red;"><button type="button" style="color:red;" class="btn btn-link">Click to Continue</button></a></center>
+</ul><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+
+
+        
+
+        
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="js/Open.js"></script>
+
+       
+
+            
+
+          
+            
+
+      
+
+
+
+    </body>
+
+</html>
